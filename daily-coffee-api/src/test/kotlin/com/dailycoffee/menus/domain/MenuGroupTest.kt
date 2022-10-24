dd48757932bee7297104aa155f56171cd8c3fd47
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-24 16:13:37 +0900

    test(menu-group): 메뉴그룹명 올바르지 않을 경우 등록할 수 없다

--- daily-coffee-api/src/test/kotlin/com/dailycoffee/menus/domain/MenuGroupTest.kt
+++ daily-coffee-api/src/test/kotlin/com/dailycoffee/menus/domain/MenuGroupTest.kt
@@ -1,8 +1,11 @@
 package com.dailycoffee.menus.domain
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EmptySource
 
 internal class MenuGroupTest {
 
@@ -10,10 +13,17 @@ internal class MenuGroupTest {
     fun `메뉴 그룹을 등록할 수 있다`() {
         val actual = MenuGroup("추천")
 
-        Assertions.assertThat(actual).isNotNull
+        assertThat(actual).isNotNull
         assertAll(
-            { Assertions.assertThat(actual.id).isNotNull },
-            { Assertions.assertThat(actual.name).isEqualTo("추천") },
+            { assertThat(actual.id).isNotNull },
+            { assertThat(actual.name).isEqualTo("추천") },
         )
+    }
+
+    @EmptySource
+    @ParameterizedTest
+    fun `메뉴 그룹의 이름이 올바르지 않으면 등록할 수 없다`(name: String) {
+        assertThatIllegalArgumentException()
+            .isThrownBy { MenuGroup(name) }
     }
 }
