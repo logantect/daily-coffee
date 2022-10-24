efa1eb217a358fcf02a7053b20fcbbc87b78282f
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-24 12:51:37 +0900

    refactoring(displayedName): 상품 이름 비속어 검증 클라이언트 기본 생성자에 포함

--- daily-coffee-api/src/main/kotlin/com/dailycoffee/products/domain/DisplayedName.kt
+++ daily-coffee-api/src/main/kotlin/com/dailycoffee/products/domain/DisplayedName.kt
@@ -1,5 +1,10 @@
 package com.dailycoffee.products.domain
 
 data class DisplayedName(
-    private val name: String
-)
+    private val name: String,
+    private val profanityClient: ProfanityClient
+) {
+    init {
+        require(!profanityClient.containsProfanity(name))
+    }
+}
