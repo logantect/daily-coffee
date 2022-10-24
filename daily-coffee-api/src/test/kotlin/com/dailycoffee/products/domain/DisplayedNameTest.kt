4b9aafbace2286a980a778a1adad0d88d9ec065c
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-24 12:51:37 +0900

    refactoring(displayedName): 상품 이름 비속어 검증 클라이언트 기본 생성자에 포함

--- daily-coffee-api/src/test/kotlin/com/dailycoffee/products/domain/DisplayedNameTest.kt
+++ daily-coffee-api/src/test/kotlin/com/dailycoffee/products/domain/DisplayedNameTest.kt
@@ -1,5 +1,6 @@
 package com.dailycoffee.products.domain
 
+import com.dailycoffee.products.infra.FakeProfanityClient
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
@@ -8,16 +9,18 @@ import org.junit.jupiter.params.provider.ValueSource
 
 internal class DisplayedNameTest {
 
+    private val profanityClient = FakeProfanityClient()
+
     @Test
     fun `상품의 이름을 생성할 수 있다`() {
-        val actual = DisplayedName("아이스 아메리카노")
-        assertThat(actual).isEqualTo(DisplayedName("아이스 아메리카노"))
+        val actual = DisplayedName("아이스 아메리카노", profanityClient)
+        assertThat(actual).isEqualTo(DisplayedName("아이스 아메리카노", profanityClient))
     }
 
     @ValueSource(strings = ["비속어", "욕설이 포함된 이름"])
     @ParameterizedTest
     fun `상품의 이름이 올바르지 않으면 등록할 수 없다`(name: String) {
         assertThatIllegalArgumentException()
-            .isThrownBy { DisplayedName(name) }
+            .isThrownBy { DisplayedName(name, profanityClient) }
     }
 }
