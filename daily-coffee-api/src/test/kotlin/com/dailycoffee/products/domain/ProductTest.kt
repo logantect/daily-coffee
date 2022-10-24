66ceec785fb285b0ffe8325aaf2a4a350b56b50e
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-24 12:23:30 +0900

    refactor(product): 상품의 가격 Price 값 객체로 변경

--- daily-coffee-api/src/test/kotlin/com/dailycoffee/products/domain/ProductTest.kt
+++ daily-coffee-api/src/test/kotlin/com/dailycoffee/products/domain/ProductTest.kt
@@ -1,7 +1,6 @@
 package com.dailycoffee.products.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import java.math.BigDecimal
@@ -11,18 +10,12 @@ internal class ProductTest {
 
     @Test
     fun `상품을 등록할 수 있다`() {
-        val actual = Product(UUID.randomUUID(), "아이스 아메리카노", BigDecimal.valueOf(58_000L))
+        val actual = Product(UUID.randomUUID(), "아이스 아메리카노", Price(58_000L))
         assertThat(actual).isNotNull
         assertAll(
             { assertThat(actual.id).isNotNull },
             { assertThat(actual.name).isEqualTo("아이스 아메리카노") },
             { assertThat(actual.price).isEqualTo(BigDecimal.valueOf(58_000L)) }
         )
-    }
-
-    @Test
-    fun `상품의 가격이 올바르지 않으면 등록할 수 없다`() {
-        assertThatIllegalArgumentException()
-            .isThrownBy { Product(UUID.randomUUID(), "아이스 아메리카노", BigDecimal.valueOf(-1)) }
     }
 }
