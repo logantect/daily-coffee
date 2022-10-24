fa56c4a8dcaa12a40ef0e0ba09d901bf82d87cb1
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-24 12:05:25 +0900

    style(ktlint): remove wildcard import

--- daily-coffee-api/src/main/kotlin/com/dailycoffee/products/domain/Product.kt
+++ daily-coffee-api/src/main/kotlin/com/dailycoffee/products/domain/Product.kt
@@ -1,11 +1,10 @@
 package com.dailycoffee.products.domain
 
 import java.math.BigDecimal
-import java.util.*
+import java.util.UUID
 
 class Product(
     val id: UUID,
     val name: String,
     val price: BigDecimal
-) {
-}
+)
