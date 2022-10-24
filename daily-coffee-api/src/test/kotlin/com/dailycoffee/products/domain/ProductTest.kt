bff7ee6bae03640ed8b22247b1cf5ace36f5e758
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-24 15:22:34 +0900

    test(product): 상품가격 테스트 값 조정

--- daily-coffee-api/src/test/kotlin/com/dailycoffee/products/domain/ProductTest.kt
+++ daily-coffee-api/src/test/kotlin/com/dailycoffee/products/domain/ProductTest.kt
@@ -12,12 +12,12 @@ internal class ProductTest {
 
     @Test
     fun `상품을 등록할 수 있다`() {
-        val actual = Product(UUID.randomUUID(), DisplayedName("아이스 아메리카노", profanityClient), 58_000L)
+        val actual = Product(UUID.randomUUID(), DisplayedName("아이스 아메리카노", profanityClient), 5_800L)
         assertThat(actual).isNotNull
         assertAll(
             { assertThat(actual.id).isNotNull },
             { assertThat(actual.name).isEqualTo(DisplayedName("아이스 아메리카노", profanityClient)) },
-            { assertThat(actual.price).isEqualTo(Price(58_000L)) }
+            { assertThat(actual.price).isEqualTo(Price(5_800L)) }
         )
     }
 }
