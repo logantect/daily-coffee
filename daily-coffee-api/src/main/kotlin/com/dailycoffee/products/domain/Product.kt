15c55b4693f78b604a4f9e438c3db645aab0d75e
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-24 12:23:30 +0900

    refactor(product): 상품의 가격 Price 값 객체로 변경

--- daily-coffee-api/src/main/kotlin/com/dailycoffee/products/domain/Product.kt
+++ daily-coffee-api/src/main/kotlin/com/dailycoffee/products/domain/Product.kt
@@ -1,10 +1,9 @@
 package com.dailycoffee.products.domain
 
-import java.math.BigDecimal
 import java.util.UUID
 
 class Product(
     val id: UUID,
     val name: String,
-    val price: BigDecimal
+    val price: Price
 )
