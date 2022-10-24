8bd388727e73b64789339cd84f5a125fc23bf5db
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-24 12:31:12 +0900

    test(displayedName): 상품의 이름이 올바르지 않으면 등록할 수 없다 실패 테스트 작성

--- daily-coffee-api/src/test/kotlin/com/dailycoffee/products/domain/DisplayedNameTest.kt
+++ daily-coffee-api/src/test/kotlin/com/dailycoffee/products/domain/DisplayedNameTest.kt
@@ -1,13 +1,23 @@
 package com.dailycoffee.products.domain
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class DisplayedNameTest {
 
     @Test
     fun `상품의 이름을 생성할 수 있다`() {
         val actual = DisplayedName("아이스 아메리카노")
-        Assertions.assertThat(actual).isEqualTo(DisplayedName("아이스 아메리카노"))
+        assertThat(actual).isEqualTo(DisplayedName("아이스 아메리카노"))
+    }
+
+    @ValueSource(strings = ["비속어", "욕설이 포함된 이름"])
+    @ParameterizedTest
+    fun `상품의 이름이 올바르지 않으면 등록할 수 없다`(name: String) {
+        assertThatIllegalArgumentException()
+            .isThrownBy { DisplayedName(name) }
     }
 }
