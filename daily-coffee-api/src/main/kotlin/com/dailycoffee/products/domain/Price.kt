e557535ab36fc519f914c60f0a16bd7aad963609
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-24 12:20:36 +0900

    refactor(price): remove non null validate

--- daily-coffee-api/src/main/kotlin/com/dailycoffee/products/domain/Price.kt
+++ daily-coffee-api/src/main/kotlin/com/dailycoffee/products/domain/Price.kt
@@ -1,13 +1,12 @@
 package com.dailycoffee.products.domain
 
 import java.math.BigDecimal
-import java.util.Objects
 
 data class Price(
     private val amount: BigDecimal
 ) {
     init {
-        require(Objects.nonNull(amount) && amount >= BigDecimal.ZERO)
+        require(amount >= BigDecimal.ZERO)
     }
 
     constructor(amount: Long) : this(BigDecimal.valueOf(amount))
