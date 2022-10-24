c1f0d0f46dedde787ecf52f98cd9930da5c7176d
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-24 15:44:58 +0900

    feat(product): 상품가격 변경 기능 개발

--- daily-coffee-api/src/main/kotlin/com/dailycoffee/products/domain/Product.kt
+++ daily-coffee-api/src/main/kotlin/com/dailycoffee/products/domain/Product.kt
@@ -5,7 +5,14 @@ import java.util.UUID
 class Product(
     val id: UUID,
     val name: DisplayedName,
-    val price: Price
+    price: Price = Price.ZERO
 ) {
+    var price: Price = price
+        private set
+
     constructor(id: UUID, name: DisplayedName, price: Long) : this(id, name, Price(price))
+
+    fun changePrice(price: Price) {
+        this.price = price
+    }
 }
