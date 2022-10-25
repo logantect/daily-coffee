18abd70be2cea07bae6904536eb0357b50855d01
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-25 12:50:39 +0900

    test(order): 배달 주소가 올바르지 않으면 배달 주문을 등록할 수 없다 실패 테스트 작성

--- daily-coffee-api/src/test/kotlin/com/dailycoffee/deliveryorders/domain/OrderTest.kt
+++ daily-coffee-api/src/test/kotlin/com/dailycoffee/deliveryorders/domain/OrderTest.kt
@@ -2,10 +2,13 @@ package com.dailycoffee.deliveryorders.domain
 
 import com.dailycoffee.utils.IdGenerator
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.assertj.core.api.SoftAssertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EmptySource
 import java.math.BigDecimal
 import java.util.UUID
 
@@ -18,7 +21,10 @@ internal class OrderTest {
 
         @Test
         fun `1개 이상의 등록된 메뉴로 배달 주문을 등록할 수 있다`() {
-            val actual = order(listOf(orderLineItem(IdGenerator.createId(), BigDecimal.valueOf(4_500L), 1)))
+            val actual = order(
+                "서울특별시 강남구 논현로 656",
+                listOf(orderLineItem(IdGenerator.createId(), BigDecimal.valueOf(4_500L), 1))
+            )
 
             assertThat(actual).isNotNull
             SoftAssertions().apply {
@@ -26,10 +32,22 @@ internal class OrderTest {
                 assertThat(actual.orderLineItems.orderLineItems).hasSize(1)
             }.assertAll()
         }
+
+        @ParameterizedTest
+        @EmptySource
+        fun `배달 주소가 올바르지 않으면 배달 주문을 등록할 수 없다`(deliveryAddress: String) {
+            assertThatIllegalArgumentException()
+                .isThrownBy {
+                    order(
+                        deliveryAddress,
+                        listOf(orderLineItem(IdGenerator.createId(), BigDecimal.valueOf(4_500L), 1))
+                    )
+                }
+        }
     }
 
-    fun order(orderLineItems: List<OrderLineItem>): Order {
-        return Order(orderLineItems)
+    fun order(deliveryAddress: String, orderLineItems: List<OrderLineItem>): Order {
+        return Order(deliveryAddress, orderLineItems)
     }
 
     fun orderLineItem(menuId: UUID, price: BigDecimal, quantity: Long): OrderLineItem {
