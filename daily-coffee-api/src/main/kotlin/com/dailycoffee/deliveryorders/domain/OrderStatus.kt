d31f5e331bf8d171e6d4c718a1f3e09ca38b3c3a
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-25 16:05:36 +0900

    feat(order): 주문을 배달한다

--- daily-coffee-api/src/main/kotlin/com/dailycoffee/deliveryorders/domain/OrderStatus.kt
+++ daily-coffee-api/src/main/kotlin/com/dailycoffee/deliveryorders/domain/OrderStatus.kt
@@ -1,7 +1,7 @@
 package com.dailycoffee.deliveryorders.domain
 
 enum class OrderStatus {
-    WAITING, ACCEPTED, SERVED, COMPLETED;
+    WAITING, ACCEPTED, SERVED, DELIVERING, DELIVERED, COMPLETED;
 
     fun isWaiting(): Boolean {
         return this == WAITING
