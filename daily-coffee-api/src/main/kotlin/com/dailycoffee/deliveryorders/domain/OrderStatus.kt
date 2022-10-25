8e452ea238e3a5444cd566d779d4afc2dc9eabd1
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-25 15:52:13 +0900

    feat(order): 접수 대기 중인 주문만 접수할 수 있다

--- daily-coffee-api/src/main/kotlin/com/dailycoffee/deliveryorders/domain/OrderStatus.kt
+++ daily-coffee-api/src/main/kotlin/com/dailycoffee/deliveryorders/domain/OrderStatus.kt
@@ -1,5 +1,9 @@
 package com.dailycoffee.deliveryorders.domain
 
 enum class OrderStatus {
-    WAITING, COMPLETED, SERVED, ACCEPTED
+    WAITING, COMPLETED, SERVED, ACCEPTED;
+
+    fun isWaiting(): Boolean {
+        return this == WAITING
+    }
 }
