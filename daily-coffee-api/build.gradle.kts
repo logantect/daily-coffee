9d06a7d8f230d02c56d67a11d978a281521d9b53
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-26 12:36:34 +0900

    build(dependencies): rest-assured 의존성 추가

--- daily-coffee-api/build.gradle.kts
+++ daily-coffee-api/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    testImplementation("io.rest-assured:kotlin-extensions:5.1.1")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java")
 }
