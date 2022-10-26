514c5a1e3525436c571d5dd249557266fcebc6c7
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-26 12:36:15 +0900

    build(dependencies): spring-boot-starter-validation 의존성 추가

--- daily-coffee-api/build.gradle.kts
+++ daily-coffee-api/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(project(":daily-coffee-support:common"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java")
 }
