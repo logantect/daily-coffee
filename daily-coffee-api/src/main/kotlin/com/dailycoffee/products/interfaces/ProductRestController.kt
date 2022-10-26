f4ec2a8fd109cf4bbcfe32a33fbcf3c9a8e90771
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-26 15:05:38 +0900

    feat(product): 상품 생성 API 응답 ApiResponse 객체로 리팩토링

--- daily-coffee-api/src/main/kotlin/com/dailycoffee/products/interfaces/ProductRestController.kt
+++ daily-coffee-api/src/main/kotlin/com/dailycoffee/products/interfaces/ProductRestController.kt
@@ -3,6 +3,7 @@ package com.dailycoffee.products.interfaces
 import com.dailycoffee.products.application.CreateProductRequest
 import com.dailycoffee.products.application.ProductResponse
 import com.dailycoffee.products.application.ProductService
+import com.dailycoffee.support.interfaces.ApiResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -16,9 +17,9 @@ class ProductRestController(
     private val productService: ProductService
 ) {
     @PostMapping
-    fun create(@RequestBody request: CreateProductRequest): ResponseEntity<ProductResponse> {
+    fun create(@RequestBody request: CreateProductRequest): ResponseEntity<ApiResponse<ProductResponse>> {
         val product = productService.create(request)
         return ResponseEntity.created(URI.create("/api/v1/products/${product.id}"))
-            .body(product)
+            .body(ApiResponse.success(product))
     }
 }
