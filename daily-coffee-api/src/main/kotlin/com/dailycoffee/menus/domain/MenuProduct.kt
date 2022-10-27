decf659fe91280b621d866e737f199adec918861
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-27 11:20:40 +0900

    feat(menuGroup): 메뉴 JPA @Entity 적용

--- daily-coffee-api/src/main/kotlin/com/dailycoffee/menus/domain/MenuProduct.kt
+++ daily-coffee-api/src/main/kotlin/com/dailycoffee/menus/domain/MenuProduct.kt
@@ -3,12 +3,19 @@ package com.dailycoffee.menus.domain
 import com.dailycoffee.support.utils.IdGenerator
 import java.math.BigDecimal
 import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Embedded
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
 
+@Entity
+@Table(name = "menu_product")
 class MenuProduct(
-    val id: UUID,
-    val productId: UUID,
-    val price: Price,
-    val quantity: Quantity,
+    @Id @Column(columnDefinition = "BINARY(16)") val id: UUID,
+    @Column(columnDefinition = "BINARY(16)") val productId: UUID,
+    @Embedded val price: Price,
+    @Embedded val quantity: Quantity,
 ) {
     constructor(productId: UUID, price: BigDecimal, quantity: Long) : this(
         IdGenerator.createId(),
--- daily-coffee-api/src/main/kotlin/com/dailycoffee/menus/domain/Quantity.kt
+++ daily-coffee-api/src/main/kotlin/com/dailycoffee/menus/domain/Quantity.kt
@@ -1,9 +1,13 @@
 package com.dailycoffee.menus.domain
 
+import javax.persistence.Column
+import javax.persistence.Embeddable
+
 private const val MINIMUM_QUANTITY_NUMBER = 0L
 
+@Embeddable
 data class Quantity(
-    val quantity: Long
+    @Column(nullable = false) val quantity: Long
 ) {
     init {
         require(quantity >= MINIMUM_QUANTITY_NUMBER)
