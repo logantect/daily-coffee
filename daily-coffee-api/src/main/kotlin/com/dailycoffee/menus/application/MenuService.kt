d79e529acf93045f6af9f44b5d7b6015134fd9b1
jeongjaeeom <jeongjae.eom@gmail.com>
2022-10-31 15:21:13 +0900

    feat(menu): 메뉴의 가격을 변경할 수 있다

--- daily-coffee-api/src/main/kotlin/com/dailycoffee/menus/application/MenuService.kt
+++ daily-coffee-api/src/main/kotlin/com/dailycoffee/menus/application/MenuService.kt
@@ -3,8 +3,11 @@ package com.dailycoffee.menus.application
 import com.dailycoffee.menus.domain.Menu
 import com.dailycoffee.menus.domain.MenuProduct
 import com.dailycoffee.menus.domain.MenuRepository
+import com.dailycoffee.menus.domain.Price
+import com.dailycoffee.menus.domain.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
 
 @Transactional
 @Service
@@ -26,5 +29,11 @@ class MenuService(
             }
         )
         return MenuResponse(menuRepository.save(menu))
+    }
+
+    fun changePrice(id: UUID, command: ChangePriceRequest) {
+        val menu = menuRepository.findByIdOrNull(id)
+            ?: throw NoSuchElementException("메뉴가 존재하지 않습니다. id: $id")
+        menu.changePrice(Price(command.price))
     }
 }
