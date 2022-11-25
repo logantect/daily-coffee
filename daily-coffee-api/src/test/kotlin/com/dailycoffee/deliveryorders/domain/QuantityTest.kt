6f7c8e9ad07e0ef67ce255d304ec0f9049501fbd
jeongjaeeom <jeongjae.eom@gmail.com>
2022-11-25 12:45:18 +0900

    test(order): 주문수량 1이상 수량만 가능하도록 개선

--- daily-coffee-api/src/test/kotlin/com/dailycoffee/deliveryorders/domain/QuantityTest.kt
+++ daily-coffee-api/src/test/kotlin/com/dailycoffee/deliveryorders/domain/QuantityTest.kt
@@ -2,14 +2,16 @@ package com.dailycoffee.deliveryorders.domain
 
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 @DisplayName("주문 항목 수량 테스트")
 internal class QuantityTest {
 
-    @Test
-    fun `주문 항목의 수량은 0 이상이어야 한다`() {
+    @ParameterizedTest
+    @ValueSource(longs = [0L, -1L])
+    fun `주문 항목의 수량은 1 이상이어야 한다`(quantity: Long) {
         assertThatIllegalArgumentException()
-            .isThrownBy { Quantity(-1) }
+            .isThrownBy { Quantity(quantity) }
     }
 }
