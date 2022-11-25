271c033b2a0d2a1a433346ad5bf0ac8ea188e9cd
jeongjaeeom <jeongjae.eom@gmail.com>
2022-11-25 12:07:26 +0900

    test(order): 주문 통합 테스트 리팩토링

---
+++ daily-coffee-api/src/test/kotlin/com/dailycoffee/BaseTests.kt
@@ -0,0 +1,17 @@
+package com.dailycoffee
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestConstructor
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@ActiveProfiles("test")
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+annotation class TestEnvironment
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestEnvironment
+annotation class IntegrationTest
--- daily-coffee-api/src/test/kotlin/com/dailycoffee/deliveryorders/interfaces/OrderRestControllerTest.kt
+++ daily-coffee-api/src/test/kotlin/com/dailycoffee/deliveryorders/interfaces/OrderRestControllerTest.kt
@@ -1,9 +1,19 @@
 package com.dailycoffee.deliveryorders.interfaces
 
+import com.dailycoffee.IntegrationTest
 import com.dailycoffee.deliveryorders.domain.OrderRepository
 import com.dailycoffee.deliveryorders.domain.OrderStatus
+import com.dailycoffee.menu
+import com.dailycoffee.menuGroup
+import com.dailycoffee.menuProduct
+import com.dailycoffee.menus.domain.MenuGroupRepository
+import com.dailycoffee.menus.domain.MenuRepository
 import com.dailycoffee.orderLineItemsRequest
 import com.dailycoffee.orderRequest
+import com.dailycoffee.product
+import com.dailycoffee.products.domain.DisplayedName
+import com.dailycoffee.products.domain.ProductRepository
+import com.dailycoffee.products.domain.ProfanityClient
 import io.restassured.RestAssured
 import io.restassured.http.ContentType
 import io.restassured.module.kotlin.extensions.Given
@@ -15,20 +25,20 @@ import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.notNullValue
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.TestConstructor
 import java.math.BigDecimal
-import java.util.UUID
 
-@ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@IntegrationTest
 class OrderRestControllerTest(
     @LocalServerPort private val port: Int,
-    private val orderRepository: OrderRepository
+    private val orderRepository: OrderRepository,
+    private val productRepository: ProductRepository,
+    private val menuGroupRepository: MenuGroupRepository,
+    private val menuRepository: MenuRepository,
+    private val profanityClient: ProfanityClient,
 ) {
     @BeforeEach
     internal fun setUp() {
@@ -41,30 +51,46 @@ class OrderRestControllerTest(
         orderRepository.deleteAll()
     }
 
-    @Test
-    fun `주문 요청에 응답으로 201 Created와 생성된 주문을 반환한다`() {
-        val menuId = UUID.randomUUID()
-        val request = orderRequest(
-            "서울특별시 강남구 논현로 656",
-            listOf(
-                orderLineItemsRequest(menuId, BigDecimal.valueOf(4_500L), 1)
-            )
-        )
+    @Nested
+    @DisplayName("1개 이상의 등록된 메뉴로")
+    inner class OrderCreate {
 
-        Given {
-            contentType(ContentType.JSON)
-            body(request)
-            log().all()
-        } When {
-            post("/api/v1/orders")
-        } Then {
-            statusCode(HttpStatus.SC_CREATED)
-            body("id", notNullValue())
-            body("status", equalTo(OrderStatus.WAITING.name))
-            body("orderDateTime", notNullValue())
-            body("deliveryAddress", equalTo("서울특별시 강남구 논현로 656"))
-            body("orderLineItems.size()", `is`(1))
-            log().all()
+        @Test
+        fun `주문 요청을 진행하면 응답으로 201 Created와 생성된 주문을 반환한다`() {
+            val product = productRepository.save(product(DisplayedName("아이스 아메리카노", profanityClient), 5_800L))
+            val menuGroup = menuGroupRepository.save(menuGroup("추천"))
+            val menu = menuRepository.save(
+                menu(
+                    "아이스 카페 아메리카노",
+                    BigDecimal.valueOf(4_500L),
+                    true,
+                    menuGroup.id,
+                    listOf(menuProduct(product.id, BigDecimal.valueOf(5_800L), 1))
+                )
+            )
+
+            val request = orderRequest(
+                "서울특별시 강남구 논현로 656",
+                listOf(
+                    orderLineItemsRequest(menu.id, BigDecimal.valueOf(4_500L), 1)
+                )
+            )
+
+            Given {
+                contentType(ContentType.JSON)
+                body(request)
+                log().all()
+            } When {
+                post("/api/v1/orders")
+            } Then {
+                statusCode(HttpStatus.SC_CREATED)
+                body("id", notNullValue())
+                body("status", equalTo(OrderStatus.WAITING.name))
+                body("orderDateTime", notNullValue())
+                body("deliveryAddress", equalTo("서울특별시 강남구 논현로 656"))
+                body("orderLineItems.size()", `is`(1))
+                log().all()
+            }
         }
     }
 }
